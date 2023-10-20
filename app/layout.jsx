4f5732ffd683552cb300a647b6de9927a81d638a
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-10-20 08:50:31 -0300

    home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 import '@styles/globals.css';
 
 export const metadata = {
@@ -9,13 +11,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
       <body>
+        <Provider>
         <div className="main">
           <div className="gradient" />
         </div>
 
         <main className="app">
+          <Nav />
           {children}
         </main>
+        </Provider>
       </body>
     </html>
   )
